83aeb9b8fc169a95f93b3198594de944a8175e52
Borisstoy <boris.stoy@gmail.com>
2018-01-21 03:26:25 -0500

    :lipstick: Fix navbar home

---
+++ app/assets/javascripts/typing.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    var typed = Typed.new('.element', {
+        strings: ["First sentence.", "Second sentence."],
+        typeSpeed: 30
+    });
+    console.log('ey')
+})
